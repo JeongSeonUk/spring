22f5dcfe25bc8aae34cc2d3438a4d786537f5f7e
JeongSeonUk <jsu4312@naver.com>
2017-01-19 17:00:35 +0900

    회원가입 폼 요청처리 완료
    resoueces 폴더에 css, js 추가함

--- Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
+++ Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.gura.spring.users.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gura.spring.users.service.UsersService;
 // component 스캔시 bean 이 되고 또 한 controller 역할을 할 수 있도록
@@ -10,4 +11,10 @@ public class UsersController {
 	// 의존객체 주입 되도록
 	@Autowired
 	private UsersService usersService;
+	
+	@RequestMapping("/users/signup_form")
+	public String signupForm(){
+		
+		return "users/signup_form";
+	}
 }
